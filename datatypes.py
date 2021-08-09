2952bbd071aec17bd17c28323a3078e12fa5fa09
burrell <eewinterz@gmail.com>
2021-08-09 02:33:09 -0400

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+# Fundamental Datatypes
+# --------------------------------
+int 
+# integers
+float
+# floating point numbers
+bool
+# boolean
+str
+# strings
+list
+# lists - mutable
+tuple
+# tuples - immutable 
+set
+# no duplicates allowed
+dict
+# key-value pairs
+
+# Classes
+# use classes to create custom types
+
+# Specialized Datatypes
+# use modules to import types
+
+# Empty Values
+None 
