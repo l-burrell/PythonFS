b74e787e369e99d5b7e9a92ecde237cc002f1043
burrell <eewinterz@gmail.com>
2021-08-09 04:15:57 -0400

    lists

---
+++ lists.py
@@ -0,0 +1,22 @@
+
+cart = [
+    'notebooks', 
+    'bags', 
+    'pencils', 
+    'laptop'
+    ]
+
+# print specific index from lists
+print(cart[0:3])
+
+# lists are mutable
+cart[2] = 'pens'
+print(cart)
+
+# create a copy of a list, then modify it
+items = cart[:]
+items[0] = "binders"
+print("items: ",items)
+
+# notice the copy doesn't reference the original
+print("cart: ",cart)
