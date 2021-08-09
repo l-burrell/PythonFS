7596ba77a4b06d969a7a30ee53f7ac3536d1b313
burrell <eewinterz@gmail.com>
2021-08-09 02:45:57 -0400

    datatypes

---
+++ test.py
@@ -0,0 +1,21 @@
+
+print("hello python")
+
+# int
+print(type(6))
+
+# division returns a float
+print(type(6/3))
+
+# integer division returns a int, because it will auto round down
+print(type(6//3))
+
+# notice: we still get a float even though we use integer division
+# this is due to us dividing by a float.
+print(type(6//3.0))
+
+# 2 ^ 3 
+print(2 ** 3)
+
+# modulo returns the remainder
+print(6 % 4)
